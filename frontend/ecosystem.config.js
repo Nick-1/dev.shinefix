a1622f0dd8e0dab6def809d017ad321da4385cef
nick <multik73@gmail.com>
2024-01-20 16:14:57 +0200

    change env

--- frontend/ecosystem.config.js
+++ frontend/ecosystem.config.js
@@ -1,10 +1,10 @@
 module.exports = {
   apps : [{
-    name: "shinefix-front",
-    script: "npm",
-    args: "run start",
-    // args: "run dev",
+    name: 'shinefix-front',
+    script: 'npm',
+    args: 'run start',
     env_production: {
+      NODE_ENV: 'production',
       NEXT_PUBLIC_BACKEND: 'http://api-v1.shinefix.pro',
     }
   }]
