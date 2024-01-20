013c0cd42094adf3542c321b988b1185c5f76bc6
nick <multik73@gmail.com>
2024-01-20 14:49:06 +0200

    change the way of build front part

--- frontend/ecosystem.config.js
+++ frontend/ecosystem.config.js
@@ -2,8 +2,8 @@ module.exports = {
   apps : [{
     name: "shinefix-front",
     script: "npm",
-    // args: "run start",
-    args: "run dev",
+    args: "run start",
+    // args: "run dev",
     env_production: {
       NEXT_PUBLIC_BACKEND: 'http://api-v1.shinefix.pro',
     }
